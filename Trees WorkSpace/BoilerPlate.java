adfd4178a5533c31bd88f9eb5c35fad68b32b3e7
Karthik Ch <saipavan39dh@gmail.com>
2023-09-30 10:57:17 +0530

    Added BoilerPlate For All WorkSpaces

---
+++ Trees WorkSpace/BoilerPlate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class BoilerPlate {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+    public static void callme()
+    {
+
+    }
+
+}
